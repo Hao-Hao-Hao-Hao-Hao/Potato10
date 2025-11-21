2b1cc523c0cfdacff1e676c427eb75f519217787
BT_2N2_25 <sample@bths.edu>
2025-11-21 10:14:39 -0500

    Potato Squeak

--- src/Potato.java
+++ src/Potato.java
@@ -1,5 +1,7 @@
 public class Potato {
     public static void main(String[] args) {
         System.out.println("I like potatoes");
+        System.out.println("Potatoes are peak");
+        System.out.println("I love Potato Squeak");
     }
 }
