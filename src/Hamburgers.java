e060fbefad28db9d7d2e80ac2d941143bb27c0ee
BT_2N2_25 <sample@bths.edu>
2025-11-21 11:01:16 -0500

    initial commit Potato Squeak Potato Squeak peak

--- src/Hamburgers.java
+++ src/Hamburgers.java
@@ -1,5 +1,6 @@
 public class Hamburgers {
     public static void main(String[] args) {
         System.out.println("hamburgers are delicious");
+        System.out.println("Me like hamburgers");
     }
 }
