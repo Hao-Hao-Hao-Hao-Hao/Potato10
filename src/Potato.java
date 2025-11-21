fc295abbd35f94845bc3e362850b62a11c5e7b41
BT_2N2_25 <sample@bths.edu>
2025-11-21 10:17:01 -0500

    initial commit Potato Squeak Potato Squeak peak

--- src/Potato.java
+++ src/Potato.java
@@ -3,5 +3,8 @@ public class Potato {
         System.out.println("I like potatoes");
         System.out.println("Potatoes are peak");
         System.out.println("I love Potato Squeak");
+        String peak = "Potato Squeak";
+        System.out.println("who is the most peak of all?");
+        System.out.println(peak);
     }
 }
