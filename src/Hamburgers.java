f2d0f6014258aca7cf3fce8fc6427b72941dcbc2
Hao Hao Wen <xiangchenwg@nycstudents.net>
2025-11-23 16:39:34 -0500

    hamburgers

--- src/Hamburgers.java
+++ src/Hamburgers.java
@@ -2,5 +2,6 @@ public class Hamburgers {
     public static void main(String[] args) {
         System.out.println("hamburgers are delicious");
         System.out.println("Me like hamburgers");
+        System.out.println("I prefer chicken over beef in hamburgers");
     }
 }
